d4cfccda690b7561674657780001e4055e819547
Caleb F <calebsf2000@gmail.com>
2019-12-12 00:16:10 -0500

    Finished OrderBook

--- orderBook.js
+++ orderBook.js
@@ -1,33 +1,27 @@
-function updatedBook(existingBook, incomingOrder) {
-  // if we can't add the order to the existing book and return the book put it in a new list
-  if (existingBook.) {
-    incomingOrder.push(updatedBook)
-    // if we can, fulfill the order add it to existing book
-
-    return updatedBook
-  }
-}
-
 function reconcileOrder(existingBook, incomingOrder) {
-  // if the existing book is empty add the order to it and return the book
-  if (existingBook.length === 0) {
+  const matchingIndex = existingBook.findIndex((order) => {
+    return incomingOrder.price === order.price && incomingOrder.type !== order.type
+  })
+
+  if (matchingIndex === -1) {
     existingBook.push(incomingOrder)
     return existingBook
-
   }
 
-  // Find corresponding orders - types are different price is the same 
+  if (incomingOrder.quantity === existingBook[matchingIndex].quantity) {
+    existingBook.splice(matchingIndex, 1)
+    return existingBook
+  }
 
-  // If no corresponding order add to book move on 
+  if (existingBook[matchingIndex].quantity > incomingOrder.quantity) {
+    existingBook[matchingIndex].quantity = existingBook[matchingIndex].quantity - incomingOrder.quantity
+    return existingBook
+  }
 
-  /* If there are corresponding orders
-     If quantities are the same remove the corresponding order from existing book and return existing books
-     If the corresponding quantitiy is greater then subtract incoming quantity from corresponding quantity and return the book
-     If the corresponding quantity is less than 
-     1 Subtract corresponding quantity from incoming quantity
-     2 Add incoming order to existing book 
-     3 Remove corresponding from existing book and return existing book
-     */
+  incomingOrder.quantity = incomingOrder.quantity - existingBook[matchingIndex].quantity
+  existingBook.splice(matchingIndex, 1)
+  existingBook.push(incomingOrder)
+  return existingBook
 }
 
 module.exports = reconcileOrder
